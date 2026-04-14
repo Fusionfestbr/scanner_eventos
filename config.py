ac4a607108e7d6fcce7daf1e1d2f8fba212c631f
fuionapp <fusionfestbr@gmail.com>
2026-04-14 20:30:47 -0300

    alteração da ia

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 LM_STUDIO_URL = os.environ.get("LM_STUDIO_URL", "http://127.0.0.1:1234/v1/chat/completions")
-MODEL = os.environ.get("MODEL", "google/gemma-4-e4b")
+MODEL = os.environ.get("MODEL", "google/gemma-3-4b")
 
 REQUEST_TIMEOUT = 120
 MAX_TOKENS = 500
